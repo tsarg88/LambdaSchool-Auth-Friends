e8e285db3a8c02ac18e94749a187b8e93679b629
umbur <you@example.com>
2019-08-20 16:41:01 -0600

    created POST request to add a new friend, MVP done

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -6,11 +6,17 @@ import axios from 'axios';
 
 class FriendsList extends React.Component {
   state = {
-    getFriends: []
+    getFriends: [],
+    postFriends: {
+        name: null,
+        age: null,
+        email: null
+      }
   };
 
   componentDidMount() {
     this.getData();
+    //this.postData();
   }
 
   getData = () => {
@@ -24,6 +30,28 @@ class FriendsList extends React.Component {
       .catch(err => console.log(err.response));
   };
 
+  postData = e => {
+    //e.preventDefault();
+    axiosWithAuth().post('http://localhost:5000/api/friends', this.state.postFriends)
+      .then(res => {
+        this.setState({
+            postFriends: res.data
+          });
+        console.log('POST from postData',res)
+        
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  handleChange = e => {
+    this.setState({
+      postFriends: {
+        ...this.state.postFriends,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
 
   render() {
     return (
@@ -35,6 +63,31 @@ class FriendsList extends React.Component {
                 <p>Email:{n.email}</p>
           </>
             ): null}
+        
+        <form onSubmit={this.postData}>
+          <input
+            type="text"
+            name="name"
+            placeholder='Name'
+            value={this.state.postFriends.name}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="age"
+            placeholder='Age'
+            value={this.state.postFriends.age}
+            onChange={this.handleChange}
+          />
+           <input
+            type="text"
+            name="email"
+            placeholder='Email'
+            value={this.state.postFriends.email}
+            onChange={this.handleChange}
+          />
+          <button>Add a Friend</button>
+        </form>
         </div>
       )
     
